3a5e4231141781106d9f5c6d1fb811b522cd7fdc
Payal <payalbhourakatoch@gmail.com>
2024-02-23 16:07:41 +0530

    todolistbackend mongo

--- models/db.js
+++ models/db.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose')
  
 const serverUrl = "mongodb://localhost:27017"
 module.exports.init = async()=>{
-    await mongoose.connect(serverUrl)
+    await mongoose.connect(process.env.MONGO_URL)
 }
